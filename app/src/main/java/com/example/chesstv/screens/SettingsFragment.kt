b5e19822f5068e3ee704b9cd936ec4f6af9ddf8a
dmitry <piervov00@gmail.com>
2022-04-29 19:08:44 +0300

    - news details skeleton

---
+++ app/src/main/java/com/example/chesstv/screens/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.chesstv.screens
+
+import androidx.fragment.app.Fragment
+import com.example.chesstv.R
+
+class SettingsFragment: Fragment(R.layout.fragment_profile_settings) {
+
+}
