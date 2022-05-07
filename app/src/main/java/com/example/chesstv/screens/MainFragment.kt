72d6588885b1d0d636697d9e3fe14eb3ce5e4936
dmitry <piervov00@gmail.com>
2022-05-07 18:25:44 +0300

    - toolbar changes

--- app/src/main/java/com/example/chesstv/screens/MainFragment.kt
+++ app/src/main/java/com/example/chesstv/screens/MainFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.chesstv.R
 import com.example.chesstv.databinding.FragmentMainBinding
 
@@ -22,6 +25,12 @@ class MainFragment: Fragment(R.layout.fragment_main) {
             as NavHostFragment
 
         val navController = navHost.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.home, R.id.streams, R.id.favorite, R.id.profile
+        ))
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
 
